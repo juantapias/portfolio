21c6f378fa849bd2d5c0d5917a61bd9f90f83a6a
Juan Tapias <dev.juantapias@gmail.com>
2024-12-08 17:14:49 -0500

    feat(APP-18): link tree

---
+++ src/app/api/link-tree/[slug]/route.ts
@@ -0,0 +1,50 @@
+import { linkTree } from '@/data/link-tree'
+import { NextResponse } from 'next/server'
+
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
+export async function GET(
+  request: Request,
+  { params }: { params: { slug: string } }
+) {
+  try {
+    const { slug } = params
+
+    if (!slug) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: 'El parámetro "slug" es requerido',
+        },
+        { status: 400 }
+      )
+    }
+
+    const user = linkTree.find(item => item.userName === slug)
+
+    if (!user) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: 'Usuario no encontrado',
+        },
+        { status: 404 }
+      )
+    }
+
+    return NextResponse.json({
+      success: true,
+      data: user,
+    })
+  } catch (error) {
+    console.error(error)
+    return NextResponse.json(
+      {
+        success: false,
+        message: 'Error al obtener los datos',
+      },
+      { status: 500 }
+    )
+  }
+}
